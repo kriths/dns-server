8cea4bc8c070d59d1a59230fbf090e4ab9b83c7e
Thomas Krisch <git@tkr.sh>
2024-04-03 23:30:26 +0200

    Catch API errors and return error to caller

--- src/handler.rs
+++ src/handler.rs
@@ -1,9 +1,18 @@
+use anyhow::bail;
 use bytes::Bytes;
-use log::{debug, trace};
+use log::{debug, error, trace};
 
 use crate::data::header::*;
 use crate::data::request::DNSRequest;
 use crate::data::response::DNSResponse;
+
+async fn handle_request(_request: &DNSRequest) -> anyhow::Result<DNSResponse> {
+    // todo: check overrides
+    // todo: check cache
+    // todo: forward to upstream
+    // todo: store in cache
+    bail!("todo");
+}
 
 pub async fn parse_and_handle_request(request_bytes: Bytes) -> anyhow::Result<DNSResponse> {
     let request = DNSRequest::from_bytes(&request_bytes)?;
@@ -12,24 +21,25 @@ pub async fn parse_and_handle_request(request_bytes: Bytes) -> anyhow::Result<DN
         request.header.identification, request.header.count_questions
     );
     trace!("Handling request {:?}", request);
-    // todo: check overrides
-    // todo: check cache
-    // todo: forward to upstream
-    // todo: store in cache
-    Ok(DNSResponse {
-        header: DNSHeader {
-            identification: request.header.identification,
-            msg_type: HeaderFlagQR::Reply,
-            opcode: request.header.opcode,
-            authoritative: request.header.authoritative,
-            truncation: false,
-            recursion_desired: false,
-            recursion_available: false,
-            response_code: ResponseCode::ServerFail,
-            count_questions: 0,
-            count_answers: 0,
-            count_authorities: 0,
-            count_additional: 0,
-        },
-    })
+
+    let response = handle_request(&request).await.unwrap_or_else(|err| {
+        error!("Error while handling request: {:?}", err);
+        DNSResponse {
+            header: DNSHeader {
+                identification: request.header.identification,
+                msg_type: HeaderFlagQR::Reply,
+                opcode: request.header.opcode,
+                authoritative: request.header.authoritative,
+                truncation: false,
+                recursion_desired: false,
+                recursion_available: false,
+                response_code: ResponseCode::ServerFail,
+                count_questions: 0,
+                count_answers: 0,
+                count_authorities: 0,
+                count_additional: 0,
+            },
+        }
+    });
+    Ok(response)
 }
